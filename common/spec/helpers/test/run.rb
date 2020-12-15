ilam-natarajan/dependabot-core

Sanitize creds from DependabotError
All errors now inherit from `DependabotError` and sanitize the message
from url encoded http basic.

common/spec/helpers/test/run.rb
@@ -7,6 +7,9 @@ case request["function"]
 when "error"
   $stdout.write(JSON.dump(error: "Something went wrong"))
   exit 1
+when "sensitive_error"
+  $stdout.write(JSON.dump(error: "Something went wrong: https://Aladdin:OpenSesame@www.example.com"))
+  exit 1
 when "useful_error"
   $stderr.write("Some useful error")
   exit 1

umahari/depend

common/spec/helpers/test/run.rb
@@ -7,6 +7,9 @@ case request["function"]
 when "error"
   $stdout.write(JSON.dump(error: "Something went wrong"))
   exit 1
+when "sensitive_error"
+  $stdout.write(JSON.dump(error: "Something went wrong: https://username:secret@www.example.com"))
+  exit 1
 when "useful_error"
   $stderr.write("Some useful error")
   exit 1